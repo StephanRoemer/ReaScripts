07d1e071225738af86804d7508e089e7f9f63d41
Stephan <info@stephanroemer.net>
2017-12-13 03:46:44 +0100

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Stephan Römer's scripts for Reaper
+
+New to Reaper and a scripting n00b, but loving the scripting API. So, here you will find scripts, I am working on.
+
+These scripts can be added via ReaPack into Reaper. Here's the repository URL:
---
+++ Transport/sr_Edit cursor follows start of item.lua
@@ -0,0 +1,31 @@
+-- @description sr_Edit cursor follows start of item
+-- @version 1.0    
+-- @author Stephan Römer
+-- @about
+--   # Edit cursor follows start of item
+--	  - this script mimics the behaviour of Nuendo's Edit Mode. You select an item, execute the script and move/nudge the item with the mouse. As result, the edit cursor will follow. 
+--    - the script is executed as an endless loop, so you have to terminate it, when you are done. Best is, to assign it as a shortcut and terminate it when pressing the shortcut again.
+--
+-- @link https://forums.cockos.com/showpost.php?p=1909014&postcount=6
+--
+-- @changelog
+-- 	  + Initial release
+
+
+loopcount=0
+
+function runloop()
+  
+  loopcount=loopcount+1
+  if loopcount >= 1 then
+    loopcount=0
+    item = reaper.GetSelectedMediaItem(0, 0)
+    item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+    reaper.SetEditCurPos(item_pos, true, false)
+  end
+  
+  reaper.defer(runloop) 
+
+end
+
+reaper.defer(runloop)
