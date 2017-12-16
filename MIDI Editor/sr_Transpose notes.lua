8e329850413f46d2fbe1b4c7a9f42c881dbaa80f
Stephan <info@stephanroemer.net>
2017-12-16 03:51:13 +0100

    test

--- MIDI Editor/sr_Transpose notes.lua
+++ MIDI Editor/sr_Transpose notes.lua
@@ -2,6 +2,6 @@
 -- @version 1.0
 -- @author Stephan Römer
 -- @metapackage
--- @provides [main=main,midi_editor,midi_inlineeditor] 
--- sr_Transpose notes *.lua [nomain] sr_Transpose function.lua
+-- @provides [main=main,midi_editor,midi_inlineeditor] sr_Transpose notes *.lua 
+--    [nomain] sr_Transpose function.lua
 
