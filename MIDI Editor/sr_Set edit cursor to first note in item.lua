3b2ecde66b6936ade5dc8f56bcde722174be1c85
Stephan <info@stephanroemer.net>
2017-12-18 06:15:51 +0100

    new commands

---
+++ MIDI Editor/sr_Set edit cursor to first note in item.lua
@@ -0,0 +1,23 @@
+-- @description Set edit cursor to first note in selected items
+-- @version 1.0
+-- @author Stephan Römer
+-- @about
+--    # Description
+--    - set the editor cursor to the beginning of the first note in selected items
+--    - this script works in arrangement, MIDI Editor and Inline Editor
+--
+-- @link https://forums.cockos.com/showthread.php?p=1923923
+--
+-- @provides [main=main,midi_editor,midi_inlineeditor] .
+-- @changelog
+--     v1.0 (2017-12-18)
+-- 	   + Initial release
+
+
+item = reaper.GetSelectedMediaItem(0, 0) -- get first item
+take = reaper.GetTake(item, 0) -- get first take
+if reaper.TakeIsMIDI(take) then -- make sure, that take is MIDI
+	_, _, _, noteStart, _, _, _, _ = reaper.MIDI_GetNote(take, 0) -- get start of first note
+	noteStartSec = reaper.MIDI_GetProjTimeFromPPQPos(take, noteStart) -- convert noteStart (PPQ) to project time
+	reaper.SetEditCurPos(noteStartSec, false, false) -- set edit cursor to note start of first note
+end
---
+++ MIDI Editor/sr_Set edit cursor to last note in item.lua
@@ -0,0 +1,26 @@
+-- @description Set edit cursor to last note in selected items
+-- @version 1.0
+-- @author Stephan Römer
+-- @about
+--    # Description
+--    - set the editor cursor to the beginning of the last note in selected items
+--    - this script works in arrangement, MIDI Editor and Inline Editor
+--
+-- @link https://forums.cockos.com/showthread.php?p=1923923
+--
+-- @provides [main=main,midi_editor,midi_inlineeditor] .
+-- @changelog
+--     v1.0 (2017-12-18)
+-- 	   + Initial release
+
+
+countItems = reaper.CountSelectedMediaItems(0)-1 -- count items
+item = reaper.GetSelectedMediaItem(0, countItems) -- get last item
+countTakes = reaper.CountTakes(item)-1 -- count takes
+take = reaper.GetTake(item, countTakes) -- get last takes
+if reaper.TakeIsMIDI(take) then -- make sure, that take is MIDI
+	notes = reaper.MIDI_CountEvts(take) -- count notes and save amount to notes
+	_, _, _, _, noteEnd, _, _, _ = reaper.MIDI_GetNote(take, notes-1) -- get end of last note
+	noteEndProj = reaper.MIDI_GetProjTimeFromPPQPos(take, noteEnd) -- convert noteEnd (PPQ) to project time
+	reaper.SetEditCurPos(noteEndProj, false, false) -- set edit cursor to note end of last note
+end
