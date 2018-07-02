fcccac4681ac576093b8eed35b3ed4db317e25eb
Stephan <info@stephanroemer.net>
2018-07-02 16:56:47 +0200

    updated: move items to timecode

--- Items Editing/sr_Move items to timecode (in filename).lua
+++ Items Editing/sr_Move items to timecode (in filename).lua
@@ -5,12 +5,14 @@
 --    # Description
 --    - this script moves either all or selected items to their timecode position (written in the filename)
 --	  - the timecode needs to be in the format xx.xx.xx.xx (Windows does not allow : in filenames)
---	  - this script is useful for audio formats, that can't carry BWF data in Reaper (FLAC for example)
+--	  - this script is useful for audio formats, where Reaper can't read the BWF chunk (FLAC for example)
 --
 -- @link https://forums.cockos.com/showthread.php?p=1923923
 --
 -- @provides [main=main] .
 -- @changelog
+--     v1.1 (2018-07-02)
+--	   + added a case to ignore files with no timecode in the filename
 --     v1.0 (2018-07-02)
 --     + Initial release
 
@@ -20,11 +22,15 @@ if reaper.CountSelectedMediaItems(0) == 0 then
 		take = reaper.GetActiveTake(item)  -- get active take in item
 		takeName = reaper.GetTakeName(take)  -- get take name
 		timecodeDot = string.match(takeName, '%d%d%.%d%d%.%d%d%.%d%d') -- match timecode
-		timecode = string.gsub(timecodeDot, "%.", ":")  -- replace "." with ":"
-		reaperTime = reaper.parse_timestr_len(timecode, 0, 5)  -- convert timecode to Reaper time
-		projectStart = reaper.GetProjectTimeOffset(0, false) -- get project start
-		newPosition = reaperTime - projectStart -- mind the project start and calculate new position!
-		reaper.SetMediaItemPosition(item, newPosition, true)-- move item to timecode
+		if timecodeDot == nil then -- if there is no timecode in the filename, skip item
+			i=i+1
+		else -- if there is a timecode in the filename
+			timecode = string.gsub(timecodeDot, "%.", ":")  -- replace "." with ":"
+			reaperTime = reaper.parse_timestr_len(timecode, 0, 5)  -- convert timecode to Reaper time
+			projectStart = reaper.GetProjectTimeOffset(0, false) -- get project start
+			newPosition = reaperTime - projectStart -- mind the project start and calculate new position!
+			reaper.SetMediaItemPosition(selectedItem, newPosition, true)-- move item to timecode
+		end
 	end
 else	
 	for i = 0, reaper.CountSelectedMediaItems(0)-1 do -- loop through all selected items
@@ -32,10 +38,14 @@ else
 		take = reaper.GetActiveTake(selectedItem)  -- get active take in item
 		takeName = reaper.GetTakeName(take)  -- get take name
 		timecodeDot = string.match(takeName, '%d%d%.%d%d%.%d%d%.%d%d') -- match timecode
-		timecode = string.gsub(timecodeDot, "%.", ":")  -- replace "." with ":"
-		reaperTime = reaper.parse_timestr_len(timecode, 0, 5)  -- convert timecode to Reaper time
-		projectStart = reaper.GetProjectTimeOffset(0, false) -- get project start
-		newPosition = reaperTime - projectStart -- mind the project start and calculate new position!
-		reaper.SetMediaItemPosition(selectedItem, newPosition, true)-- move item to timecode
+		if timecodeDot == nil then -- if there is no timecode in the filename, skip item
+			i=i+1 -- if there is a timecode in the filename
+		else
+			timecode = string.gsub(timecodeDot, "%.", ":")  -- replace "." with ":"
+			reaperTime = reaper.parse_timestr_len(timecode, 0, 5)  -- convert timecode to Reaper time
+			projectStart = reaper.GetProjectTimeOffset(0, false) -- get project start
+			newPosition = reaperTime - projectStart -- mind the project start and calculate new position!
+			reaper.SetMediaItemPosition(selectedItem, newPosition, true)-- move item to timecode
+		end
 	end
 end
