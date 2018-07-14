56e05dea131d5beed217cf6162fb436b77d0579e
Stephan <info@stephanroemer.net>
2018-07-14 02:37:20 +0200

    updated: change note position to grid (mousewheel)

--- MIDI Editor/sr_Change note position to grid (mousewheel).lua
+++ MIDI Editor/sr_Change note position to grid (mousewheel).lua
@@ -1,5 +1,5 @@
 -- @description Change note position to grid (mousewheel)
--- @version 1.12
+-- @version 1.2
 -- @author Stephan Römer
 -- @about
 --    # Description
@@ -12,6 +12,8 @@
 --
 -- @provides [main=midi_editor] .
 -- @changelog
+--     v1.2 (2018-07-14)
+--     + the script now reacts based on the grid set in the MIDI editor
 --     v1.12 (2018-07-13)
 --     + added undo text
 --     v1.11 (2018-07-12)
@@ -21,40 +23,70 @@
 
 _,_,_,_,_,_, val = reaper.get_action_context() -- receive mousewheel action
 take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive()) -- get current take opened in editor
+grid, _, _ = reaper.MIDI_GetGrid(take)
+gridPPQ = reaper.MIDI_GetPPQPosFromProjQN(take, grid)
 _, _, _ = reaper.BR_GetMouseCursorContext() -- initiate "get mouse cursor context"
 _, _, noteRow, _, _, _ = reaper.BR_GetMouseCursorContext_MIDI() -- get note row under mouse cursor
 mouse_ppq_pos = reaper.MIDI_GetPPQPosFromProjTime(take, reaper.BR_GetMouseCursorContext_Position()) -- get mouse position in project time and convert to PPQ
 notesCount, _, _ = reaper.MIDI_CountEvts(take) -- count notes in current take
-snap =  reaper.GetToggleCommandStateEx(32060, 1014) -- get snap toggle state from MIDI editor, 32060 is the MIDI editor section_id
+snap = reaper.MIDIEditor_GetSetting_int(reaper.MIDIEditor_GetActive(), "snap_enabled" ) -- get snap toggle state from MIDI editor
+allQuantized = true -- initialize variable, expect all notes quantized true, until proven otherwise. See below for explanation
+
 
 -- are there selected notes?
 if reaper.MIDI_EnumSelNotes(take, -1) ~= -1 then -- check, if there are selected notes
 	notesSelected = true -- set notesSelected to true
 end
 
-for n = 0, notesCount - 1 do
+-- are all selected notes quantized? This is needed when nudging a group of notes to the left. This ensures that all notes are moved (quantized) to the previous grid first, before moving them alltogether,
+-- otherwise they will fall apart and loose "synch".
+for n = 0, notesCount - 1 do -- loop through notes 
+		_, selectedOut, _, startppqposOut, endppqposOut, _, pitch, _ = reaper.MIDI_GetNote(take, n) -- get note selection status, start/end position and pitch
+	if tonumber(string.match(startppqposOut, "%d+")) % tonumber(string.match(gridPPQ, "%d+")) ~= 0 and selectedOut == true then -- if modulo is not 0 (not all selected notes are on the grid)
+		allQuantized = false -- set allQuantized false
+	end
+end
+
+for n = 0, notesCount - 1 do -- loop through notes
 	_, selectedOut, _, startppqposOut, endppqposOut, _, pitch, _ = reaper.MIDI_GetNote(take, n) -- get note selection status, start/end position and pitch
 	if snap == 1 then -- if snap is on
 		if notesSelected == true then -- if there are selected notes
 			if selectedOut == true then -- is current note selected?
-				noteStart = reaper.MIDI_GetProjTimeFromPPQPos(take, startppqposOut) -- convert note start from PPQ to project time 
 				if val > 0 then -- if mousewheel up
-					nextGridPPQ = reaper.MIDI_GetPPQPosFromProjTime(take, reaper.BR_GetNextGridDivision(noteStart)) -- get next grid division in project time and convert next grid division back to PPQ
-					reaper.MIDI_SetNote(take, n, nil, nil, nextGridPPQ, nextGridPPQ+endppqposOut-startppqposOut, nil, nil, nil, true) -- set new position, endposition = nextGridPPQ (offset) + endppqposOut-startppqposOut (length)
-				else 			-- if mousewheel down
-					prevGridPPQ = reaper.MIDI_GetPPQPosFromProjTime(take, reaper.BR_GetPrevGridDivision(noteStart)) -- get previous grid division in project time and convert previous grid division back to PPQ
-					reaper.MIDI_SetNote(take, n, nil, nil, prevGridPPQ, prevGridPPQ+endppqposOut-startppqposOut, nil, nil, nil, true) -- set new position, endposition = prevGridPPQ (offset) + endppqposOut-startppqposOut (length)
+					if tonumber(string.match(startppqposOut, "%d+")) % tonumber(string.match(gridPPQ, "%d+")) == 0 then -- if modulo from startppqposOut and gridPPQ equals 0, note sits already on the grid
+						reaper.MIDI_SetNote(take, n, nil, nil, startppqposOut+gridPPQ, endppqposOut+gridPPQ, nil, nil, nil, true) -- set new position = forward one grid position
+					else -- note does not sit on the grid
+						modulo = tonumber(string.match(startppqposOut, "%d+")) % tonumber(string.match(gridPPQ, "%d+")) -- calculate modulo rest (remove .0 from PPQ string and convert to number, only that way the modulo operator can be used)
+						newPosition = gridPPQ - modulo -- calculate new position (distance to gridPPQ)
+						reaper.MIDI_SetNote(take, n, nil, nil, startppqposOut+newPosition, endppqposOut+newPosition, nil, nil, nil, true) -- set new position, startppqposOut+newPosition = on the grid
+					end
+				else -- if mousewheel down
+					if allQuantized == false then -- this is a special case, for notes being nudged to the left. Not all notes are quantized. See the comment above (first for-loop). 
+						modulo = tonumber(string.match(startppqposOut, "%d+")) % tonumber(string.match(gridPPQ, "%d+")) -- get modulo rest
+						reaper.MIDI_SetNote(take, n, nil, nil, startppqposOut-modulo, endppqposOut-modulo, nil, nil, nil, true) -- quantize notes to the left grid, until all notes are quantized
+					elseif tonumber(string.match(startppqposOut, "%d+")) % tonumber(string.match(gridPPQ, "%d+")) == 0 and allQuantized == true then -- if modulo from startppqposOut and gridPPQ equals 0, note sits already on the grid and all notes are quantized
+						reaper.MIDI_SetNote(take, n, nil, nil, startppqposOut-gridPPQ, endppqposOut-gridPPQ, nil, nil, nil, true) -- set new position, startppqposOut+newPosition = on the grid
+					end
 				end
 			end
 		else -- if there are no selected notes, only change the position of the note under the mouse cursor
 			if startppqposOut < mouse_ppq_pos and endppqposOut > mouse_ppq_pos and noteRow == pitch then -- is current note the note under the cursor?
 				noteStart = reaper.MIDI_GetProjTimeFromPPQPos(take, startppqposOut) -- convert note start from PPQ to project time 
 				if val > 0 then -- if mousewheel up
-					nextGridPPQ = reaper.MIDI_GetPPQPosFromProjTime(take, reaper.BR_GetNextGridDivision(noteStart)) -- get next grid division in project time and convert next grid division back to PPQ
-					reaper.MIDI_SetNote(take, n, nil, nil, nextGridPPQ, nextGridPPQ+endppqposOut-startppqposOut, nil, nil, nil, true) -- set new position, endposition = nextGridPPQ (offset) + endppqposOut-startppqposOut (length)
-				else 			-- if mousewheel down
-					prevGridPPQ = reaper.MIDI_GetPPQPosFromProjTime(take, reaper.BR_GetPrevGridDivision(noteStart)) -- get previous grid division in project time and convert previous grid division back to PPQ
-					reaper.MIDI_SetNote(take, n, nil, nil, prevGridPPQ, prevGridPPQ+endppqposOut-startppqposOut, nil, nil, nil, true) -- set new position, endposition = prevGridPPQ (offset) + endppqposOut-startppqposOut (length)
+					if tonumber(string.match(startppqposOut, "%d+")) % tonumber(string.match(gridPPQ, "%d+")) == 0 then -- if modulo from startppqposOut and gridPPQ equals 0, note sits already on the grid
+						reaper.MIDI_SetNote(take, n, nil, nil, startppqposOut+gridPPQ, endppqposOut+gridPPQ, nil, nil, nil, true) -- set new position, forward one position in grid
+					else -- note does not sit on the grid
+						modulo = tonumber(string.match(startppqposOut, "%d+")) % tonumber(string.match(gridPPQ, "%d+")) -- remove .0 from PPQ string and convert to number, only that way the modulo operator can be used
+						newPosition = gridPPQ - modulo
+						reaper.MIDI_SetNote(take, n, nil, nil, startppqposOut+newPosition, endppqposOut+newPosition, nil, nil, nil, true) -- set new position
+					end
+				else -- if mousewheel down
+					if tonumber(string.match(startppqposOut, "%d+")) % tonumber(string.match(gridPPQ, "%d+")) == 0 then -- if modulo from startppqposOut and gridPPQ equals 0, note sits already on the grid
+						reaper.MIDI_SetNote(take, n, nil, nil, startppqposOut-gridPPQ, endppqposOut-gridPPQ, nil, nil, nil, true) -- set new position, 
+					else 
+						modulo = tonumber(string.match(startppqposOut, "%d+")) % tonumber(string.match(gridPPQ, "%d+"))
+						reaper.MIDI_SetNote(take, n, nil, nil, startppqposOut-modulo, endppqposOut-modulo, nil, nil, nil, true) -- set new position, endposition = prevGridPPQ (offset) + endppqposOut-startppqposOut (length)
+					end
 				end
 			end
 		end
