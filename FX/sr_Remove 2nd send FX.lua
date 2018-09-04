9b5b194f4d39a291baa8875f1b55df87c6997fd6
Stephan <info@stephanroemer.net>
2018-09-04 19:37:22 +0200

    new scripts: Remove send FX

---
+++ FX/sr_Remove 2nd send FX.lua
@@ -0,0 +1,29 @@
+-- @description Remove 2nd send FX
+-- @version 1.0
+-- @author Stephan Römer
+-- @about
+--    # Description
+--    - removes the 2nd send FX in the send slot of selected track(s)
+--
+-- @link https://forums.cockos.com/showthread.php?p=1923923
+--
+-- @provides [main=main] .
+-- @changelog
+--     	v1.0 (2018-09-04)
+-- 	   	+ Initial release
+
+reaper.Undo_BeginBlock()
+
+if reaper.CountSelectedTracks(0) == 0 then
+    reaper.ShowMessageBox("Please select at least one track", "Error", 0)
+else
+    local selected_tracks = reaper.CountSelectedTracks(0)
+    for i = 0, selected_tracks-1 do -- loop thru all selected tracks
+        local track = reaper.GetSelectedTrack(0, i) -- get current selected track
+        reaper.RemoveTrackSend(track, 0, 1) -- remove 2nd send of selected track(s)
+    end
+end
+
+reaper.Undo_EndBlock("Remove 2nd send FX", 1)
+
+
