11e1495701d06dece3646f4db93d94cf97e07840
Stephan Römer <Stevie@stephans-macbook.speedport.ip>
2018-09-08 19:02:31 +0200

    update

--- FX/sr_Remove send 2 from selected track(s).lua
+++ FX/sr_Remove send 2 from selected track(s).lua
@@ -1,7 +1,7 @@
 -- @noindex
 
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_Remove send from selected tracks function'
+require 'sr_Remove send from selected track(s) function'
 
 local send_number = 2
 
