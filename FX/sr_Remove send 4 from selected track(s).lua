bbbbe42e34f36ca24d883966c00f5c11c2c6ecc6
Stephan Römer <Stevie@stephans-macbook.speedport.ip>
2018-09-08 17:23:29 +0200

    Remove send bundle

--- FX/sr_Remove send 4 from selected track(s).lua
+++ FX/sr_Remove send 4 from selected track(s).lua
@@ -1,19 +1,3 @@
--- @description Remove send from selected track(s)
--- @version 1.1
--- @changelog
---   switched to external function file and put all scripts in a bundle
--- @author Stephan Römer
--- @provides
--- 	. > sr_Remove send from selected track(s) function.lua
--- 	. > sr_Remove send 1 from selected track(s).lua
--- 	. > sr_Remove send 2 from selected track(s).lua
--- 	. > sr_Remove send 3 from selected track(s).lua
--- 	. > sr_Remove send 4 from selected track(s).lua
--- @about
---   # Description
---   This script bundle consists of 4 scripts that remove sends 1-4 from the selected track(s)
--- @link Forum Thread https://forums.cockos.com/showthread.php?p=1923923
-
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
 require 'sr_Remove send from selected tracks function'
 
---
+++ FX/sr_Remove send from selected track(s) bundle.lua
@@ -0,0 +1,16 @@
+-- @description Remove send from selected track(s)
+-- @version 1.1
+-- @changelog
+--   switched to external function file and put all scripts in a bundle
+-- @author Stephan Römer
+-- @metapackage
+-- @provides
+--  [nomain] sr_Remove send from selected track(s) function.lua
+--  [main] sr_Remove send 1 from selected track(s).lua
+--  [main] sr_Remove send 2 from selected track(s).lua
+--  [main] sr_Remove send 3 from selected track(s).lua
+--  [main] sr_Remove send 4 from selected track(s).lua
+-- @about
+--   # Description
+--   This script bundle consists of 4 scripts that remove sends 1-4 from the selected track(s)
+-- @link Forum Thread https://forums.cockos.com/showthread.php?p=1923923
