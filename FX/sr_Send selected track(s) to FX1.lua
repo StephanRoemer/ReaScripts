d53eae8b04f58961e806458ae6942a2df2111746
Stephan Römer <Stevie@stephans-macbook.speedport.ip>
2018-09-08 18:56:48 +0200

    update

--- FX/sr_Send selected track(s) to FX1.lua
+++ FX/sr_Send selected track(s) to FX1.lua
@@ -1,7 +1,7 @@
 -- @noindex
 
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_Send selected tracks to FX'
+require 'sr_Send selected track(s) to FX track function'
 
 local send_fx_prefix = "FX1" -- send FX1
 
--- FX/sr_Toggle show UI of all FX in send 1.lua
+++ FX/sr_Toggle show UI of all FX in send 1.lua
@@ -1,7 +1,7 @@
 -- @noindex
 
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_Toggle show UI of all FX in send slots'
+require 'sr_Toggle show UI of all FX in send function'
 
 local send_slot = 1 -- 1st send slot
 
