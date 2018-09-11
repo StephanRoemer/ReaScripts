5c055eaae89e85cbdcd0e633dd14ed814d41cc2f
Stephan <info@stephanroemer.net>
2018-09-11 17:07:12 +0200

    Changed name from Add Notes to Double notes

---
+++ MIDI Editor/sr_Double notes -24.lua
@@ -0,0 +1,9 @@
+--  @noindex
+
+package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
+require 'sr_Double notes function'
+
+interval = -24 -- semitones
+
+double_notes(interval) -- call function
+reaper.Undo_OnStateChange2(proj, "Add notes -24")
