5a9f630d698657f8decee782dad2a9b824ac5ece
Stephan <info@stephanroemer.net>
2018-09-11 15:39:55 +0200

    Transpose notes is now available in a single bundle and does not depend on MIDI functions anymore

--- MIDI Editor/sr_Transpose notes +9.lua
+++ MIDI Editor/sr_Transpose notes +9.lua
@@ -1,21 +1,4 @@
--- @description Transpose notes +9
--- @version 1.11
--- @author Stephan Römer
--- @about
---    # Description
---    - this script transposes either all notes or selected notes by 9 semitone up
---    - this script works in arrangement, MIDI Editor and Inline Editor
---
--- @link https://forums.cockos.com/showthread.php?p=1923923
---
--- @provides [main=main,midi_editor,midi_inlineeditor] .
--- @changelog
---     v1.11 (2017-12-16)
---     + added undo state
---     v1.1 (2017-12-15)
---     + moved the functions to a seperate file "sr_MIDI functions"
---     v1.0
---     + Initial release
+-- @noindex
 
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
 require 'sr_MIDI functions'
