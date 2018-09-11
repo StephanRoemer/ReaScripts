d68b2be9624f0c0c1e47d73f67ea313c9f7c44be
Stephan <info@stephanroemer.net>
2018-09-11 20:15:41 +0200

    Select CC has its own bundle now

--- MIDI Editor/sr_Select CC1.lua
+++ MIDI Editor/sr_Select CC1.lua
@@ -1,28 +1,11 @@
--- @description Select CC1
--- @version 1.11
--- @author Stephan Römer
--- @about
---    # Description
---    - this script selects the complete CC1 lane
---    - this script works in arrangement, MIDI Editor and Inline Editor
---
--- @link https://forums.cockos.com/showthread.php?p=1923923
---
--- @provides [main=main,midi_editor,midi_inlineeditor] .
--- @changelog
---     v1.11 (2017-12-16)
---     + added undo state
---     v1.1 (2017-12-15)
---     + moved the functions to a seperate file "sr_MIDI functions"
---     v1.0
---     + Initial release
+--  @noindex
 
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_MIDI functions'
+require 'sr_Select CC function'
 
 destCC = 1 -- CC
 
-select_CC(destCC) -- call function
+SelectCC(dest_cc) -- call function
 reaper.Undo_OnStateChange2(proj, "Select CC1")
 
 
