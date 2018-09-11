ac2c6e258804b7d48134929f29975d3e37d10668
Stephan <info@stephanroemer.net>
2018-09-11 17:54:45 +0200

    Nudge notes now is a bundle and includes its own function

---
+++ MIDI Editor/sr_Nudge notes (bundle).lua
@@ -0,0 +1,18 @@
+-- @description Nudge notes (bundle)
+-- @version 1.30
+-- @changelog
+--   The scripts are now available in a single bundle and the MIDI functions.lua is not necessary anymore. Instead the bundle includes its own external function file.  
+-- @author Stephan Römer
+-- @metapackage
+-- @provides
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge notes left by 10 ticks.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge notes right by 10 ticks.lua
+--  [nomain] sr_Nudge notes (bundle).lua
+--  [nomain] sr_Nudge notes function.lua
+-- @about
+--    # Description
+--    * This script bundles consists of 2 scripts that nudges either all notes or selected notes left/right.
+--    * The amount of ticks can be adjusted in the scripts. That way, you can easily create different versions
+--      that fit your personal needs. 
+--    * The scripts work in arrangement, MIDI Editor and Inline Editor
+-- @link Forum Thread https://forums.cockos.com/showthread.php?p=1923923
---
+++ MIDI Editor/sr_Nudge notes function.lua
@@ -0,0 +1,41 @@
+--  @noindex
+
+function NudgeNotes(new_position)
+    
+    for i = 0, reaper.CountSelectedMediaItems(0)-1 do -- loop through all selected items
+		item = reaper.GetSelectedMediaItem(0, i) -- get current selected item
+    
+        for t = 0, reaper.CountTakes(item)-1 do -- loop through all takes within each selected item
+			take = reaper.GetTake(item, t) -- get current take
+    
+            if reaper.TakeIsMIDI(take) then -- make sure, that take is MIDI
+				if reaper.MIDI_EnumSelNotes(take, -1) ~= -1 then -- check, if there are selected notes
+					notesSelected = true -- set notesSelected to true
+				end
+				gotAllOK, MIDIstring = reaper.MIDI_GetAllEvts(take, "") -- write MIDI events to MIDIstring, get all events okay
+    
+                if not gotAllOK then reaper.ShowMessageBox("Error while loading MIDI", "Error", 0) return(false) end -- if getting the MIDI data failed
+				MIDIlen = #MIDIstring -- get string length
+				tableEvents = {} -- initialize table, MIDI events will temporarily be stored in this table until they are concatenated into a string again
+				stringPos = 1 -- position in MIDIstring while parsing through events 
+    
+                while stringPos < MIDIlen-12 do -- parse through all events in the MIDI string, one-by-one, excluding the final 12 bytes, which provides REAPER's All-notes-off end-of-take message
+					offset, flags, msg, stringPos = string.unpack("i4Bs4", MIDIstring, stringPos) -- unpack MIDI-string on stringPos
+    
+                    if #msg == 3 
+					and ((msg:byte(1)>>4) == 9 or (msg:byte(1)>>4) == 8) -- Is note-on or note-off?
+					and (flags&1 == 1 or not notesSelected) -- selected notes always move, unselected only move if no notes are selected
+					then
+    					table.insert(tableEvents, string.pack("i4Bs4", offset+newPosition, flags, msg)) -- move the note on event by newPosition
+    					table.insert(tableEvents, string.pack("i4Bs4", -newPosition, 0, "")) -- put an empty event after the note on event, to maintain the distance
+					else
+    					table.insert(tableEvents, string.pack("i4Bs4", offset, flags, msg))
+					end
+				end
+			end
+		end
+	end
+	reaper.MIDI_SetAllEvts(take, table.concat(tableEvents) .. MIDIstring:sub(-12))
+	reaper.MIDI_Sort(take)
+	reaper.UpdateArrange()
+end
