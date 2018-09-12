b0ee69ba2f88397015413fc53c1e9ed677dd4157
Stephan <info@stephanroemer.net>
2018-09-12 21:56:31 +0200

    Increase CC has its own bundle now

--- MIDI Editor/sr_Increase CC11.lua
+++ MIDI Editor/sr_Increase CC11.lua
@@ -1,28 +1,10 @@
--- @description Increase CC11
--- @version 1.1
--- @author Stephan Römer
--- @about
---    # Description
---    - this script increases all or selected CC events in the CC11 lane
---    - this script works in arrangement, MIDI Editor and Inline Editor
---
--- @link https://forums.cockos.com/showthread.php?p=1923923
---
--- @provides [main=main,midi_editor,midi_inlineeditor] .
--- @changelog
---     v1.1 (2017-12-16)
---     + added undo state
---     v1.0
---     + Initial release
+--  @noindex
 
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_MIDI functions'
+require 'sr_Increase CC function'
 
 increase = 1.1 -- value to increase the CC event
-destCC = 11 -- destination CC
+dest_cc = 11 -- destination CC
 
-increase_CC(destCC, increase) -- call function
+IncreaseCC(dest_cc, increase) -- call function
 reaper.Undo_OnStateChange2(proj, "Increase CC11")
-
-
-
--- MIDI Editor/sr_Increase CC2.lua
+++ MIDI Editor/sr_Increase CC2.lua
@@ -4,7 +4,7 @@ package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;
 require 'sr_Increase CC function'
 
 increase = 1.1 -- value to increase the CC event
-dest_cc = 1 -- destination CC
+dest_cc = 2 -- destination CC
 
 IncreaseCC(dest_cc, increase) -- call function
-reaper.Undo_OnStateChange2(proj, "Increase CC1")
+reaper.Undo_OnStateChange2(proj, "Increase CC2")
