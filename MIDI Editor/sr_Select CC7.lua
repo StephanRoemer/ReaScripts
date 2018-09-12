fe50eff6ab65abb3692c1b24cbd4f89feba7796d
Stephan <info@stephanroemer.net>
2018-09-12 20:49:02 +0200

    Select CC has its own bundle now

---
+++ MIDI Editor/sr_Select CC7.lua
@@ -0,0 +1,13 @@
+--  @noindex
+
+package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
+require 'sr_Select CC function'
+
+dest_cc = 7 -- CC
+
+SelectCC(dest_cc) -- call function
+reaper.Undo_OnStateChange2(proj, "Select CC7")
+
+
+
+
