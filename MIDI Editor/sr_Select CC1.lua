e2657502abc48f8d1da71f950d3372a715f05814
Stephan <info@stephanroemer.net>
2018-09-12 20:45:43 +0200

    typo fixed

--- MIDI Editor/sr_Select CC1.lua
+++ MIDI Editor/sr_Select CC1.lua
@@ -3,7 +3,7 @@
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
 require 'sr_Select CC function'
 
-destCC = 1 -- CC
+dest_cc = 1 -- CC
 
 SelectCC(dest_cc) -- call function
 reaper.Undo_OnStateChange2(proj, "Select CC1")
