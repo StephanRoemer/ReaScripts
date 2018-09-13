d9bf938aa15ae5949bded9d8f336262c7a648eb8
Stephan <info@stephanroemer.net>
2018-09-13 04:09:44 +0200

    update

--- MIDI Editor/sr_Select all CCs.lua
+++ MIDI Editor/sr_Select all CCs.lua
@@ -1,34 +1,46 @@
 -- @description Select all CCs
--- @version 1.1
--- @author Stephan Römer
+-- @version 1.30
+-- @changelog
+--   Switched to Get/SetAllEvts
+-- @author Stephan Römer, Julian Sader
+-- @provides [main=main,midi_editor,midi_inlineeditor] .
 -- @about
 --    # Description
---    - this script selects all CCs
---    - this script works in arrangement, MIDI Editor and Inline Editor
---
+--    * This script selects all CCs
+--    * This script works in arrangement, MIDI Editor and Inline Editor
 -- @link https://forums.cockos.com/showthread.php?p=1923923
---
--- @provides [main=main,midi_editor,midi_inlineeditor] .
--- @changelog
---     v1.1 (2017-12-16)
---     + added undo state
---     v1.0
---     + Initial release
 
+if reaper.CountSelectedMediaItems(0) == 0 then
+	reaper.ShowMessageBox("Please select at least one item", "Error", 0)
+	return false
+else 
+	for i = 0, reaper.CountSelectedMediaItems(0)-1 do -- loop through all selected items
+		local item = reaper.GetSelectedMediaItem(0, i) -- get current selected item
+		local take = reaper.GetActiveTake(item)
+		
+		if reaper.TakeIsMIDI(take) then -- make sure, that take is MIDI
+			gotAllOK, MIDIstring = reaper.MIDI_GetAllEvts(take, "") -- write MIDI events to MIDIstring, get all events okay
 
-for i = 0, reaper.CountSelectedMediaItems(0)-1 do -- loop through all selected items
-    item = reaper.GetSelectedMediaItem(0, i)
-    for t = 0, reaper.CountTakes(item)-1 do -- Loop through all takes within each selected item
-        take = reaper.GetTake(item, t)
-        if reaper.TakeIsMIDI(take) then -- make sure, that take is MIDI
-            _, _, ccCount, _ = reaper.MIDI_CountEvts(take) -- count CCs and save amount to "ccCount"
-            for c = 0, ccCount - 1 do -- loop thru all CCs
-                reaper.MIDI_SetCC(take, c, true, nil, nil, nil, nil, nil, nil, true) -- select CCs   
-            end
-        end
-    end
+			if not gotAllOK then reaper.ShowMessageBox("Error while loading MIDI", "Error", 0) return(false) end -- if getting the MIDI data failed
+			MIDIlen = #MIDIstring -- get string length
+			tableEvents = {} -- initialize table, MIDI events will temporarily be stored in this table until they are concatenated into a string again
+			stringPos = 1 -- position in MIDIstring while parsing through events 
+
+			while stringPos < MIDIlen-12 do -- parse through all events in the MIDI string, one-by-one, excluding the final 12 bytes, which provides REAPER's All-notes-off end-of-take message
+				offset, flags, msg, stringPos = string.unpack("i4Bs4", MIDIstring, stringPos) -- unpack MIDI-string on stringPos
+
+				if #msg == 3 then -- if msg consists of 3 bytes (= channel message)
+					msg_b1_nib1 = msg:byte(1)>>4 -- save 1st nibble of status byte to msg_b1_nib1, >>4 shifts the channel nibble into oblivion
+
+					if msg_b1_nib1 == 11 then -- if status byte is a CC
+						flags = flags|1 -- select muted or unmuted event
+					end
+				end
+				table.insert(tableEvents, string.pack("i4Bs4", offset, flags, msg)) -- re-pack MIDI string and write all other events to table
+			end
+		end
+		reaper.MIDI_SetAllEvts(take, table.concat(tableEvents) .. MIDIstring:sub(-12))
+		reaper.MIDI_Sort(take)
+	end
 end
-
-reaper.Undo_OnStateChange2(proj, "Select all CCs")
-
-
+reaper.UpdateArrange()
