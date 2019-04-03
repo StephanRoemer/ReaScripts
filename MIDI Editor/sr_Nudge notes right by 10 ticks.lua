39ce4ea1a84967eaff48d4dca3d1564eb7cb2195
Stephan Römer <stephan.roemer@gmail.com>
2019-04-03 12:10:40 +0200

    delete old version

--- MIDI Editor/sr_Nudge notes right by 10 ticks.lua
+++
@@ -1,9 +0,0 @@
--- @noindex
-
-package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_Nudge notes function'
-
-newPosition = 10 -- ticks
-
-NudgeNotes(new_position) -- call function
-reaper.Undo_OnStateChange2(proj, "Nudge notes right by 10 ticks")
