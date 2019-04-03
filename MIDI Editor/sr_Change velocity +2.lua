af46c00ea1d1ede1bc9b85f17affbc93c96f6139
Stephan Römer <stephan.roemer@gmail.com>
2019-04-03 11:27:55 +0200

    re-upload

---
+++ MIDI Editor/sr_Change velocity +2.lua
@@ -0,0 +1,9 @@
+-- @noindex
+
+package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
+require 'sr_Change velocity function'
+
+new_velocity = 2
+
+ChangeVelocity(new_velocity) -- call function
+reaper.Undo_OnStateChange2(proj, "Change velocity +2")
