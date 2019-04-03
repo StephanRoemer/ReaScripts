f02a9af787856aa8488e308d441cdd4791d45af2
Stephan Römer <stephan.roemer@gmail.com>
2019-04-03 12:13:30 +0200

    update

---
+++ MIDI Editor/sr_Transpose notes +10.lua
@@ -0,0 +1,9 @@
+-- @noindex
+
+package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
+require 'sr_Transpose notes function'
+
+interval = 10 -- semitones
+
+Transpose(interval) -- call function
+reaper.Undo_OnStateChange2(proj, "Transpose notes +10")
