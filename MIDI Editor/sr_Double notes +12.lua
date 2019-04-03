eebc4e4534a420f9d6957e19a0676edf58da35e7
Stephan Römer <stephan.roemer@gmail.com>
2019-04-03 11:21:22 +0200

    delete

--- MIDI Editor/sr_Double notes +12.lua
+++
@@ -1,9 +0,0 @@
---  @noindex
-
-package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_Double notes function'
-
-interval = 12 -- semitones
-
-double_notes(interval) -- call function
-reaper.Undo_OnStateChange2(proj, "Add notes +12")
--- MIDI Editor/sr_Transpose notes +7.lua
+++
@@ -1,9 +0,0 @@
--- @noindex
-
-package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_Transpose notes function'
-
-interval = 7 -- semitones
-
-Transpose(interval) -- call function
-reaper.Undo_OnStateChange2(proj, "Transpose notes +7")
