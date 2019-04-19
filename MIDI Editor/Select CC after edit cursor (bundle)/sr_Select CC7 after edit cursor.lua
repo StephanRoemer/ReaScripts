9bcb104772461bfe08ad89ede93e6febc2cd87db
Stephan Römer <stephan.roemer@gmail.com>
2019-04-20 01:53:37 +0200

    update

--- MIDI Editor/Select CC after edit cursor (bundle)/sr_Select CC7 after edit cursor.lua
+++
@@ -1,9 +0,0 @@
---  @noindex
-
-package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_Select CC after edit cursor function'
-
-local dest_cc = 7 -- CC
-
-SelectCCAfterEditCursor(dest_cc) -- call function
-reaper.Undo_OnStateChange2(proj, "Select CC7 after edit cursor")
--- MIDI Editor/Select CC before edit cursor (bundle)/sr_Select CC11 before edit cursor.lua
+++
@@ -1,9 +0,0 @@
---  @noindex
-
-package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-require 'sr_Select CC before edit cursor function'
-
-local dest_cc = 11 -- CC
-
-SelectCCBeforeEditCursor(dest_cc) -- call function
-reaper.Undo_OnStateChange2(proj, "Select CC11 before edit cursor")
