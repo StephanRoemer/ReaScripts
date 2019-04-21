aca508ff391a30254a5e94fd4edd1b67688341df
Stephan Römer <stephan.roemer@gmail.com>
2019-04-21 19:45:15 +0200

    update

--- MIDI Editor/Quantize note end to closest grid (bundle)/sr_Quantize note end to closest grid (bundle).lua
+++ MIDI Editor/Quantize note end to closest grid (bundle)/sr_Quantize note end to closest grid (bundle).lua
@@ -1,4 +1,4 @@
--- @description Quantize note end to next grid (bundle)
+-- @description Quantize note end to closest grid (bundle)
 -- @version 1.0
 -- @changelog
 --   + initial release
@@ -18,7 +18,7 @@
 --  [nomain] sr_Quantize note end to closest grid function.lua
 -- @about
 --    # Description
---    * This script quantizes note end (to next grid) of either all notes or selected notes in one or multiple items.
+--    * This script quantizes note end (to closest grid) of either all notes or selected notes in one or multiple items.
 --    * In the arrange view (item view), selected events are not taken into account, because you can't see what is selected.
 --    * In the inline editor, you MUST hover the mouse over the active inline editor, otherwise ALL events will 
 --    be affected, instead of only the selected ones.
