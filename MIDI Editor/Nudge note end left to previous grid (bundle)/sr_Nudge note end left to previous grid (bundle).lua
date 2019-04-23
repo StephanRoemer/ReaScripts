50cdab289327f272691096dd8b9acd28af64601a
Stephan Römer <stephan.roemer@gmail.com>
2019-04-23 15:41:47 +0200

    update

---
+++ MIDI Editor/Nudge note end left to previous grid (bundle)/sr_Nudge note end left to previous grid (bundle).lua
@@ -0,0 +1,28 @@
+-- @description Nudge note end left to previous grid (bundle)
+-- @version 1.0
+-- @changelog
+--   * script renamed to a more meaningful name (previously: Quantize note end to previous grid)
+-- @author Stephan Römer
+-- @metapackage
+-- @provides
+--  [nomain] .
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge note end left to previous grid - 1-1.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge note end left to previous grid - 1-2.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge note end left to previous grid - 1-4.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge note end left to previous grid - 1-4 triplets.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge note end left to previous grid - 1-8.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge note end left to previous grid - 1-8 triplets.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge note end left to previous grid - 1-16.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge note end left to previous grid - 1-16 triplets.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Nudge note end left to previous grid - grid.lua
+--  [nomain] sr_Nudge note end left to previous grid function.lua
+-- @about
+--    # Description
+--    * This script nudges the note end (to previous grid) of either all notes or selected notes in one or multiple items.
+--    * In the arrange view (item view), selected events are not taken into account, because you can't see what is selected.
+--    * In the inline editor, you MUST hover the mouse over the active inline editor, otherwise ALL events will 
+--    be affected, instead of only the selected ones.
+--    * When hovering an inline editor, only the take under the mouse cursor will be affected, regardles of the item selection.
+--    * You can easily customize the values in the scripts.
+--    * The scripts work in the MIDI editor, inline editor and arrange view.
+-- @link https://forums.cockos.com/showthread.php?p=1923923
---
+++ MIDI Editor/Nudge note end left to previous grid (bundle)/sr_Nudge note end left to previous grid - 1-2.lua
@@ -0,0 +1,14 @@
+--  @noindex
+
+package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
+require 'sr_Nudge note end left to previous grid function'
+
+local grid = 1/2 -- 1/2 grid
+local swing = 0 -- swing off
+local swing_amt = 0 -- swing amount
+
+local _, save_project_grid, save_swing, save_swing_amt = reaper.GetSetProjectGrid(proj, false) -- backup current grid settings
+reaper.GetSetProjectGrid(proj, true, grid, swing, swing_amt) -- set new grid settings according variable grid, swing and swing_amt
+NudgeNoteEndLeft() -- call function
+reaper.GetSetProjectGrid(proj, true, save_project_grid, save_swing, save_swing_amt) -- restore saved grid settings
+reaper.Undo_OnStateChange2(proj, "Nudge note end left to previous grid - 1/2")
