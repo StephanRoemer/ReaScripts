2269767eadf50234753521f595129f7fff5074f9
Stephan Römer <stephan.roemer@gmail.com>
2019-04-24 01:06:36 +0200

    update

---
+++ MIDI Editor/Quantize note start to closest grid (bundle)/sr_Quantize note start to closest grid (bundle).lua
@@ -0,0 +1,28 @@
+-- @description Quantize note start to closest grid (bundle)
+-- @version 1.0
+-- @changelog
+--   * initial release
+-- @author Stephan Römer
+-- @metapackage
+-- @provides
+--  [nomain] .
+--  [main=main,midi_editor,midi_inlineeditor] sr_Quantize note start to closest grid - 1-1.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Quantize note start to closest grid - 1-2.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Quantize note start to closest grid - 1-4.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Quantize note start to closest grid - 1-4 triplets.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Quantize note start to closest grid - 1-8.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Quantize note start to closest grid - 1-8 triplets.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Quantize note start to closest grid - 1-16.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Quantize note start to closest grid - 1-16 triplets.lua
+--  [main=main,midi_editor,midi_inlineeditor] sr_Quantize note start to closest grid - grid.lua
+--  [nomain] sr_Quantize note start to closest grid function.lua
+-- @about
+--    # Description
+--    * This script quantizes note start (to closest grid) of either all notes or selected notes in one or multiple items.
+--    * In the arrange view (item view), selected events are not taken into account, because you can't see what is selected.
+--    * In the inline editor, you MUST hover the mouse over the active inline editor, otherwise ALL events will 
+--    be affected, instead of only the selected ones.
+--    * When hovering an inline editor, only the take under the mouse cursor will be affected, regardles of the item selection.
+--    * You can easily customize the values in the scripts.
+--    * The scripts work in the MIDI editor, inline editor and arrange view.
+-- @link https://forums.cockos.com/showthread.php?p=1923923
---
+++ MIDI Editor/Quantize note start to closest grid (bundle)/sr_Quantize note start to closest grid function.lua
@@ -0,0 +1,110 @@
+--  @noindex
+
+function QuantizeNoteStart()
+
+
+    -- quantize take in MIDI/inline editor (respect note selection)
+
+    local function QuantizeNoteStartMIDIEditor(take)
+
+        _, notes_count, _, _ = reaper.MIDI_CountEvts(take) -- count notes and save amount to notes_count
+        
+        if reaper.MIDI_EnumSelNotes(take, -1) ~= -1 then notes_selected = true end -- check, if there are selected notes, set notes_selected to true
+
+        for n = 0, notes_count - 1 do -- loop through all notes
+            _, selected, _, note_start_pos_ppq, note_end_pos_ppq, _, _, _ = reaper.MIDI_GetNote(take, n) -- get note data
+
+            note_start = reaper.MIDI_GetProjTimeFromPPQPos(take, note_start_pos_ppq) -- convert note start to seconds
+            closest_grid = reaper.BR_GetClosestGridDivision(note_start) -- get closest grid for current note (return value in seconds)
+            closest_grid_ppq = reaper.MIDI_GetPPQPosFromProjTime(take, closest_grid) -- convert closest grid to PPQ
+            
+            if selected or not notes_selected then -- selected notes always move, unselected only move if no notes are selected
+                if not (note_end_pos_ppq - closest_grid_ppq < 1) then -- if new note length is bigger than 1 tick
+                    
+                    reaper.MIDI_SetNote(take, n, nil, nil, closest_grid_ppq, nil, nil, nil, nil, true) -- Quantize note start to the closest grid
+                    
+                else -- if note length change would result in a length of 1 tick, extend note to previous grid
+                    
+                    note_start = reaper.MIDI_GetProjTimeFromPPQPos(take, note_start_pos_ppq) -- convert note start to seconds
+                    prev_grid = reaper.BR_GetPrevGridDivision(note_start) -- get previous grid for current note (return value in seconds)
+                    prev_grid_ppq = reaper.MIDI_GetPPQPosFromProjTime(take, prev_grid) -- convert prev_grid to PPQ
+
+                    reaper.MIDI_SetNote(take, n, nil, nil, prev_grid_ppq, nil, nil, nil, nil, true) -- Quantize note start to previous grid
+                    
+                end
+            end
+        end
+        reaper.MIDI_Sort(take)
+    end
+
+
+    -- quantize selected item(s) in arrange view (ignore note selection)
+
+    local function QuantizeNoteStartArrange(take)
+        
+        _, notes_count, _, _ = reaper.MIDI_CountEvts(take) -- count notes and save amount to notes_count
+
+        for n = 0, notes_count - 1 do -- loop through all notes
+            _, _, _, note_start_pos_ppq, note_end_pos_ppq, _, _, _ = reaper.MIDI_GetNote(take, n) -- get note data
+            
+            note_start = reaper.MIDI_GetProjTimeFromPPQPos(take, note_start_pos_ppq) -- convert note start to seconds
+            closest_grid = reaper.BR_GetClosestGridDivision(note_start) -- get closest grid for current note (return value in seconds)
+            closest_grid_ppq = reaper.MIDI_GetPPQPosFromProjTime(take, closest_grid) -- convert closest grid to PPQ
+
+            if not (note_end_pos_ppq - closest_grid_ppq < 1) then -- if new note length is bigger than 1 tick
+
+                reaper.MIDI_SetNote(take, n, nil, nil, closest_grid_ppq, nil, nil, nil, nil, true) -- Quantize note start to closest grid
+        
+            else -- if note length change would result in a length of 1 tick, extend note to previous grid
+                    
+                note_start = reaper.MIDI_GetProjTimeFromPPQPos(take, note_start_pos_ppq) -- convert note start to seconds
+                prev_grid = reaper.BR_GetPrevGridDivision(note_start) -- get previous grid for current note (return value in seconds)
+                prev_grid_ppq = reaper.MIDI_GetPPQPosFromProjTime(take, prev_grid) -- convert prev_grid to PPQ
+                reaper.MIDI_SetNote(take, n, nil, nil, nil, prev_grid_ppq, nil, nil, nil, true) -- Quantize note start
+                    
+            end
+        end
+        reaper.MIDI_Sort(take)
+    end
+
+
+    -- check, where the user wants to change notes: MIDI editor, inline editor or anywhere else
+
+    local take, item    
+    local window, _, _ = reaper.BR_GetMouseCursorContext() -- initialize cursor context
+    local _, inline_editor, _, _, _, _ = reaper.BR_GetMouseCursorContext_MIDI() -- check if mouse hovers an inline editor
+    
+    if window == "midi_editor" then -- MIDI editor focused
+
+        if not inline_editor then -- not hovering inline editor
+            take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive()) -- get take from active MIDI editor
+            
+        else -- hovering inline editor (will ignore item selection and only change data in the hovered inline editor)
+            take = reaper.BR_GetMouseCursorContext_Take() -- get take from mouse
+            
+        end
+        QuantizeNoteStartMIDIEditor(take) -- Quantize note start
+            
+    else -- anywhere else (apply to selected items in arrane view)
+        
+        if reaper.CountSelectedMediaItems(0) ~= 0 then
+            for i = 0, reaper.CountSelectedMediaItems(0)-1 do -- loop through all selected items
+                item = reaper.GetSelectedMediaItem(0, i) -- get current selected item
+                take = reaper.GetActiveTake(item)
+                
+                if reaper.TakeIsMIDI(take) then -- make sure, that take is MIDI
+                    
+                    QuantizeNoteStartArrange(take) -- Quantize note start
+
+                else
+                    reaper.ShowMessageBox("The selected item #".. i+1 .." does not contain a MIDI take and won't be altered", "Error", 0)
+                end
+            end
+        
+        else
+            reaper.ShowMessageBox("Please select at least one item", "Error", 0)
+            return false
+        end
+    end
+    reaper.UpdateArrange()
+end
