45cd0a48e7f4b1a5c8a3ae2ad1f3b9692d52d374
Stephan Römer <stephan.roemer@gmail.com>
2019-04-28 14:55:49 +0200

    update add/replace VSTi

---
+++ Tracks Properties/Add or replace VSTi/sr_Add or replace existing VSTi with Zebra 2.lua
@@ -0,0 +1,20 @@
+-- @noindex
+
+package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
+require 'sr_Add or replace VSTi function'
+
+local vsti = "Zebra2 (u-he)" -- VSTi identifier
+local plugin_name = "Zebra 2" -- plugin name
+
+
+reaper.Undo_BeginBlock()
+
+local replaced = AddInstrument(vsti, plugin_name) -- call function an get replaced variable, relevant for undo text
+
+if replaced then
+    undo_text = "Replace existing VSTi with "
+else
+    undo_text = "Add "
+end
+
+reaper.Undo_EndBlock(undo_text..plugin_name, 0)
