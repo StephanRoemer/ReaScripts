d8166dd76837c8b950e8dd54f080bd2c571bba89
StephanRoemer <34496643+StephanRoemer@users.noreply.github.com>
2021-06-21 13:20:54 +0200

    Add files via upload

--- Transport/sr_Go to end marker.lua
+++ Transport/sr_Go to end marker.lua
@@ -1,24 +1,29 @@
 -- @description Go to end marker
--- @version 1.0
+-- @version 1.1
 -- @changelog
---   initial release
+--  + Code optimizations
 -- @author Stephan Römer
--- @provides [main=main] .
+-- @link https://forums.cockos.com/showthread.php?p=1923923
+-- @provides [main] .
 -- @about
 --    # Description
 --    * This script puts the edit / play cursor to the marker named "=END"
 --    * I use this in conjunction with "X-Raym_Insert or update start and end marker from time selection.lua" to create the project boundaries
+--    or my own script "sr_Set start and end marker to items in project.lua" (both available via ReaPack)
 --    * This script works only in the arrangement
--- @link https://forums.cockos.com/showthread.php?p=1923923
 
 
-marker_num = reaper.CountProjectMarkers(0)
+local marker_num = reaper.CountProjectMarkers(0)
 
 for i=0, marker_num-1 do
-    _, _, pos, _, name, _ = reaper.EnumProjectMarkers(i)
+    local _, _, pos, _, name, _ = reaper.EnumProjectMarkers(i)
     if name == "=END" then
-        reaper.GoToMarker(0, i+1, true)
+        reaper.SetEditCurPos(pos, true, false)
     end
+end
+
+if reaper.GetPlayState() == 1 then -- if playback is on
+    reaper.OnPlayButton() -- press play to move the play cursor to the edit cursor
 end
 
 reaper.UpdateArrange()
